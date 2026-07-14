55b0e2fb15d2da997d5733c4f3d3d36218a4e8dc
akash-374 <avulaa325@gmail.com>
2026-07-14 07:46:44 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+// --- PRODUCT DATABASE ---
+const productsList = [
+  {
+    id: 1,
+    name: "Fresh Avocados (2 pcs)",
+    price: 120,
+    image: "https://cdn-icons-png.flaticon.com/512/10565/10565572.png",
+    desc: "Butter fruit, rich in healthy fats.",
+  },
+  {
+    id: 2,
+    name: "Whole Wheat Bread",
+    price: 45,
+    image: "https://cdn-icons-png.flaticon.com/512/2271/2271089.png",
+    desc: "Freshly baked whole wheat bread.",
+  },
+  {
+    id: 3,
+    name: "Cow Milk (1L)",
+    price: 70,
+    image: "https://cdn-icons-png.flaticon.com/512/2405/2405451.png",
+    desc: "Pasteurized fresh cow milk.",
+  },
+  {
+    id: 4,
+    name: "Dark Chocolate",
+    price: 150,
+    image: "https://cdn-icons-png.flaticon.com/512/2553/2553691.png",
+    desc: "70% Cocoa Dark Chocolate.",
+  },
+];
+
+// --- USER DATABASE (For Login) ---
+const usersList = [
+  { email: "user@test.com", password: "1234" },
+  { email: "admin@zepto.com", password: "admin" },
+  { email: "rahul@gmail.com", password: "rahul" },
+];
